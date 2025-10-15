0867dfa71dc55777626078e82e1792099581245b
Riyaz Sayyad <riyaz@Riyazs-MacBook-M4.local>
2025-10-15 23:42:12 +0530

    Added

--- tests/Chapter03/05_Paremeterized_Test.spec.ts
+++ tests/Chapter03/05_Paremeterized_Test.spec.ts
@@ -1,22 +1,27 @@
-// import the playwright module
+// Import playwright module
 import { test, expect } from '@playwright/test';
 
-// write test case
-test('Validating the page title', async ({ page }) => {
-    console.log("Test Started")
-    // launch the browser and open a new google page
-    await page.goto('https://demowebshop.tricentis.com/');
+const searchKeywords = ['test@qabrains.com', 'practice@qabrains.com', 'student@qabrains.com'];
 
-    // Validate the web page title
-    await expect(page).toHaveTitle('Demo Web Shop');
+for (const searchKeyword of searchKeywords) {
 
-    await page.locator('[class="ico-login"]').click();
-    await page.locator('.email').fill('admin01@gmail.com');
-    await page.locator('.password').fill('admin01');
-    await page.locator('[value="Log in"]').click();
-    await page.locator('.ico-logoutt').click();
-    await page.close();
+    test(`Parameterize Tests in Playwright ${searchKeyword}`, async ({ page }) => {
+        // Go to URL
+        await page.goto('https://practice.qabrains.com/ecommerce/login');
 
-    console.log("Test Ended");
+        // Search with keywords
+        await page.locator('#email').fill(searchKeyword);
+        await page.locator('#password').fill('Password123');
+        await page.locator('[type="submit"]').click();
 
-})
+
+        await expect(page.getByRole('heading', { name: 'Products' })).toBeVisible();
+        await expect(page.getByText('Order by')).toBeVisible();
+
+        await page.locator('[class="caret font-bold"]').click();
+        await page.getByRole('button', { name: 'Log out' }).click();
+        await page.getByRole('button', { name: 'Logout' }).click();
+
+
+    });
+}

78220f2829474ed2382ee539dc92dd3518cad075
Riyaz Sayyad <riyaz@Riyazs-MacBook-M4.local>
2025-10-16 23:29:56 +0530

    Added some files

---
+++ tests/Chapter03/06_VisualTesting_Test.spec.ts
@@ -0,0 +1,36 @@
+// import the playwright module
+import { test, expect } from '@playwright/test';
+
+// write test case
+test('Visual Comparison in Playwright', async ({ page }) => {
+
+    console.log("Test Started")
+
+    await page.goto('https://github.com/login');
+
+    await expect(page).toHaveScreenshot('GitHubLoginPage.png');
+
+    await page.locator('#login_field').fill('riyajsayyad');
+    await page.locator('#password').fill('pass@1234');
+
+    await expect(page).toHaveScreenshot('GitHubLoginPage.png');
+
+    console.log("Test Ended");
+
+})
+
+// write test case
+test('Element Visual Comparison in Playwright', async ({ page }) => {
+
+    console.log("Test Started")
+
+    await page.goto('https://github.com/login');
+
+    const form = page.locator('[action="/session"]');
+    await page.locator('#login_field').fill('riyajsayyad');
+
+    await expect(form).toHaveScreenshot('GitHubLoginForm.png');
+
+    console.log("Test Ended");
+
+})

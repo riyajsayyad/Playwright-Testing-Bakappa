e21e4285b7c6025b972c9c8b1db974ecc54c255d
Riyaz Sayyad <riyaz@Riyazs-MacBook-M4.local>
2025-10-21 12:50:18 +0530

    added on line in feature 1

--- tests/Chapter03/07_TimesOuts_Test.spec.ts
+++ tests/Chapter03/07_TimesOuts_Test.spec.ts
@@ -17,5 +17,6 @@ test('Timeout in Playwright', async ({ page }) => {
 
     console.log("Test Ended");
     console.log("Added new main branch line");
+    console.log("Created feature 2 branch ")
 
 })
